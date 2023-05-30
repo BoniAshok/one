36722f9e5a9f4869da2714917e736f8a7ed4727b
BoniAshok <127587091+BoniAshok@users.noreply.github.com>
2023-05-30 12:44:44 +0530

    Create loki.py
    a simple calculator

---
+++ loki.py
@@ -0,0 +1,19 @@
+print("select any one:" )
+print("1.multiplication:")
+print("2. division:")
+print("3.subtraction")
+print("4.addition")
+x=int(input("select any method"))
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+
+if x==1:
+    print(a*b)
+if x==2:
+    print(a/b)
+if x==3:
+    print(a-b)
+if x==4:
+    print(a+b)
+      
+      
